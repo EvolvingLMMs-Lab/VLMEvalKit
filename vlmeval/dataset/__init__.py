2e1b5afba0eea6a6b68085684c519133e93c1329
Peter_Stacy <90747373+PeterWangyi@users.noreply.github.com>
2025-11-07 22:00:40 +0800

    [Benchmark] Add EASI related video benchmark (#3)
    * [Feature] add EASI related spatial bench utils func
    
    * [Style] format using pre-commit
    
    * [Style] format using pre-commit
    
    * [Feature] upgrade unknown image format verify
    
    * [Feature] add mindcube bench
    
    * [Fix] cache path is none error during first download
    
    * [Feature] add embspatial benchmark
    
    * [Feature] add viewspatial benchmark
    
    * [Feature] add mmsi with out circular benchmark
    
    * [Feature] enable mmsi && embspatial && viewspatial evaluation
    
    * [Feature] add prepare tsv method to VideoBaseDataset
    
    * [Feature] add vsi bench
    
    * [Feature] add Site Bench
    
    * [Feature] enable vsi && site evaluation
    
    * [Fix] Sitebench category name mismatch

--- vlmeval/dataset/__init__.py
+++ vlmeval/dataset/__init__.py
@@ -61,6 +61,8 @@ from .EgoExoBench.egoexobench import EgoExoBench_MCQ
 
 from .worldsense import WorldSense
 from .qbench_video import QBench_Video, QBench_Video_MCQ, QBench_Video_VQA
+from .sitebench import SiteBenchVideo
+from .vsibench import VsiBench
 
 from .cmmmu import CMMMU
 from .emma import EMMADataset
@@ -92,6 +94,7 @@ from .mindcubebench import MindCubeBench
 from .embspatialbench import EmbSpatialBench
 from .viewspatialbench import ViewSpatialBench
 from .mmsibench import MMSIBench
+from .sitebench import SiteBenchImage
 
 from .reasonmap_plus import ReasonMap_Plus
 from .gsm8k_v import GSM8KVDataset
@@ -225,7 +228,7 @@ IMAGE_DATASET = [
     AyaVisionBench, TopViewRS, VLMBias, MMHELIX, MedqbenchMCQDataset, MathCanvas,
     MedqbenchPairedDescriptionDataset, MedqbenchCaptionDataset, ChartMuseum, ChartQAPro, ReasonMap_Plus,
     olmOCRBench, OceanOCRBench, MATBench, VLRMBench,
-    MindCubeBench, EmbSpatialBench, ViewSpatialBench, MMSIBench
+    MindCubeBench, EmbSpatialBench, ViewSpatialBench, MMSIBench, SiteBenchImage
 ]
 
 VIDEO_DATASET = [
