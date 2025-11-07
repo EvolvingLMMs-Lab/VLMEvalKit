4d28e6ed390adff06d6154c48ace8b59fb15b228
wangyubo <971454032@qq.com>
2025-11-07 15:01:58 +0800

    [Feature] Add SenseSI series models

--- vlmeval/config.py
+++ vlmeval/config.py
@@ -1853,6 +1853,15 @@ spatial_related_models = {
     ),
 }
 
+sensesi_series = {
+    "SenseSI-InternVL3-2B": partial(
+        InternVLChat, model_path="sensenova/SenseSI-InternVL3-2B", version="V2.0"
+    ),
+    "SenseSI-InternVL3-8B": partial(
+        InternVLChat, model_path="sensenova/SenseSI-InternVL3-8B", version="V2.0"
+    ),
+}
+
 internvl_groups = [
     internvl, internvl2, internvl2_5, mini_internvl, internvl2_5_mpo, 
     internvl3, internvl3_5
@@ -1880,6 +1889,7 @@ model_groups = [
 ]
 
 model_groups.append(spatial_related_models)
+model_groups.append(sensesi_series)
 
 for grp in model_groups:
     supported_VLM.update(grp)
