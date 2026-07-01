d1fe63c02c1ba9a10100fb962039aac769bd20e5
oscarqjh <oscar.jh9@gmail.com>
2026-07-01 13:56:50 +0800

    Revert "[Fix] Preserve image placeholders for frame-named files"
    This reverts commit 87a0963b55b6121f97dbdda41f52af5d6a9837e9.

--- vlmeval/vlm/neo/utils.py
+++ vlmeval/vlm/neo/utils.py
@@ -160,7 +160,7 @@ def reorganize_prompt(message, image_num, dataset=None):
                 image_idx += 1
             elif x["type"] == "text":
                 prompt += x["value"]
-            elif x["type"] == "image":
+            elif x["type"] == "image" and "frame" not in x["value"]:
                 prompt += "<image>\n"
 
     return prompt
